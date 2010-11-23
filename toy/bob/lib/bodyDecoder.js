3489368bb4d91a3f82abb3f5a8af487a049b0d0d
Stephane Alnet <stephane@shimaore.net>
2010-11-23 13:33:44 +0100

    ...now that we have a working version of bodyDecoder.js

--- toy/bob/lib/bodyDecoder.js
+++ toy/bob/lib/bodyDecoder.js
@@ -37,7 +37,6 @@ var util = require('util')
 exports = module.exports = function bodyDecoder(){
     return function bodyDecoder(req, res, next) {
         var decoder = exports.decode[mime(req)];
-        util.debug("mime="+mime(req)+",body="+util.inspect(req.body)+",length="+util.inspect(req.headers['content-length']));
         if (decoder && !req.body && req.headers['content-length']) {
             var data = '';
             req.setEncoding('utf8');
