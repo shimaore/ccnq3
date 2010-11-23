636a2ab8e8ee4a76753f3c63b99b7661b9d8df43
Stephane Alnet <stephane@shimaore.net>
2010-11-23 13:23:57 +0100

    wrong name?

--- toy/bob/lib/bodyDecoder.js
+++ toy/bob/lib/bodyDecoder.js
@@ -37,8 +37,8 @@ var util = require('util')
 exports = module.exports = function bodyDecoder(){
     return function bodyDecoder(req, res, next) {
         var decoder = exports.decode[mime(req)];
-        util.debug("mime="+mime(req)+",body="+util.inspect(req.body)+",length="+util.inspect(req['content-length']));
-        if (decoder && !req.body && req['content-length']) {
+        util.debug("mime="+mime(req)+",body="+util.inspect(req.body)+",length="+util.inspect(req['Content-Length']));
+        if (decoder && !req.body && req['Content-Length']) {
             var data = '';
             req.setEncoding('utf8');
             req.addListener('data', function(chunk) { data += chunk; });
