8df324d48e48fbc4d939ae1b8a78ca6c8d2d182e
Stephane Alnet <stephane@shimaore.net>
2011-01-26 14:41:49 +0100

    proper exporter

--- common/opensips/CCNQ/Logger.pm
+++ common/opensips/CCNQ/Logger.pm
@@ -1,6 +1,12 @@
 package CCNQ::Logger;
+use strict; use warnings;
+use Exporter;
+our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
+$VERSION = '0.1';
+@ISA = qw(Exporter);
 
-sub warning { print STDERR "Warning: ".join(' ',@_) }
-sub error   { print STDERR "Error: ".join(' ',@_) }
+sub info    { print STDERR "Info: ".join(' ',@_,)."\n" }
+sub warning { print STDERR "Warning: ".join(' ',@_)."\n" }
+sub error   { print STDERR "Error: ".join(' ',@_)."\n" }
 
-@EXPORT = qw( &warning &error );
+@EXPORT = qw( &info &warning &error );
