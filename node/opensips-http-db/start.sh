f7eb92c0e53d3fe46888a873704f8ac1e96f0ca1
Stephane Alnet <stephane@shimaore.net>
2011-01-26 22:38:46 +0100

    zappa is verbose

--- node/opensips-http-db/start.sh
+++ node/opensips-http-db/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SCRIPT_DIR=`dirname "$0"`
-daemon -n opensips-http-db -r -D ${SCRIPT_DIR}  -- zappa -p 34340 main.coffee
+daemon -n opensips-http-db -r -D ${SCRIPT_DIR} -o daemon.debug -- zappa -p 34340 main.coffee
