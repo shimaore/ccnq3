21e1f8c8cc66736676bbd16c2506557d570d11dc
Stephane Alnet <stephane@shimaore.net>
2011-01-27 21:30:20 +0100

    doc, big bold warning

--- common/mediaproxy/install.sh
+++ common/mediaproxy/install.sh
@@ -1,9 +1,50 @@
 #!/bin/bash
 
-# install (dispatcher|(relay|both) [dispatcher1 [dispatcher2 [...]]])
 
 ACTION=$1
 shift
+
+if [ "x$ACTION" == "x" ]; then
+  cat - <<'EOT'
+
+$0 (dispatcher|(relay|both) [dispatcher1 [dispatcher2 [...]]])
+
+Creates configuration files for MediaProxy.
+
+This script will also adjust your /etc/sysctl.conf file to allow
+forwarding of IPv4 and IPv6.
+
+IMPORTANT
+
+If you are using IPv6, routing will break at this point, because enabling
+IPv6 forwarding will disable autoconfiguration.
+
+Make sure you statically set both your IPv6 address and your default IPv6 route
+before running the installation for a MediaProxy relay, otherwise you WILL LOOSE
+ACCESS TO THIS MACHINE.
+
+This can be done for example by adding the following lines under your eth0 entry
+in /etc/network/interfaces:
+
+   # Replace my IPv6 addresses with yours!
+   up   ip -6 addr  add 2001:1900:201e:6:216:3eff:fe39:ce6f/64 dev eth0
+   up   ip -6 route add default via 2001:1900:201e:6::1
+   down ip -6 addr  del 2001:1900:201e:6:216:3eff:fe39:ce6f/64 dev eth0
+   down ip -6 route del default via 2001:1900:201e:6::1
+
+You should also manually run the "ip -6 route add default via .... " command
+before running this script.
+
+
+Again,
+
+    Running this script to configure a relay
+    will cause trouble with IPv6 access
+    if you do not first update your settings.
+
+EOT
+
+
 
 function do_sysctl {
   sudo sed -i -e 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
