8606b89caf43f33f033f022763d907bc55a2a5e4
Stephane Alnet <stephane@shimaore.net>
2011-01-27 21:08:25 +0100

    oops, these go into tls/

--- common/mediaproxy/install.sh
+++ common/mediaproxy/install.sh
@@ -9,9 +9,9 @@ if [ "x$ACTION" == "xdispatcher" ]; then
 
   sudo aptitude -y install mediaproxy-dispatcher
 
-  sudo mkdir -p /etc/mediaproxy/dispatcher
+  sudo mkdir -p /etc/mediaproxy/tls
   sudo cp ./dispatcher/config.ini   /etc/mediaproxy/
-  sudo cp ./dispatcher/dispatcher.* /etc/mediaproxy/dispatcher
+  sudo cp ./dispatcher/dispatcher.* /etc/mediaproxy/tls
 
   sudo /etc/init.d/mediaproxy-dispatcher restart
 
@@ -21,10 +21,10 @@ if [ "x$ACTION" == "xrelay" ]; then
 
   sudo aptitude -y install mediaproxy-relay
 
-  sudo mkdir -p /etc/mediaproxy/relay
+  sudo mkdir -p /etc/mediaproxy/tls
   sed -e "s/\${dispatcher_names}/$*/" ./relay/config.ini | \
     sudo tee /etc/mediaproxy/config.ini >/dev/null
-  sudo cp ./relay/relay.* /etc/mediaproxy/relay
+  sudo cp ./relay/relay.* /etc/mediaproxy/tls
 
   sudo /etc/init.d/mediaproxy-relay restart
 
@@ -34,13 +34,12 @@ if [ "x$ACTION" == "xboth" ]; then
 
   sudo aptitude -y install mediaproxy-dispatcher mediaproxy-relay
 
-  sudo mkdir -p /etc/mediaproxy/dispatcher
-  sudo mkdir -p /etc/mediaproxy/relay
+  sudo mkdir -p /etc/mediaproxy/tls
   cat ./*/config.ini | \
     sed -e "s/\${dispatcher_names}/$*/" | \
     sudo tee /etc/mediaproxy/config.ini >/dev/null
-  sudo cp ./dispatcher/dispatcher.* /etc/mediaproxy/dispatcher
-  sudo cp ./relay/relay.*           /etc/mediaproxy/relay
+  sudo cp ./dispatcher/dispatcher.* /etc/mediaproxy/tls
+  sudo cp ./relay/relay.*           /etc/mediaproxy/tls
 
   sudo /etc/init.d/mediaproxy-dispatcher restart
   sudo /etc/init.d/mediaproxy-relay restart
