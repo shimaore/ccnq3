3b2722f914d0eb4ffce056c5e68f99c832b46d1b
Stephane Alnet <stephane@shimaore.net>
2011-02-16 16:14:37 +0100

    problems with finding the .coffee file

--- toy/bob/start.sh
+++ toy/bob/start.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 SCRIPT_DIR=`dirname "$0"`
+if [ "x${SCRIPT_DIR}" == "x." ]; then
+  SCRIPT_DIR=`pwd`
+fi
+
 daemon -n sotel-video-portal -r -D ${SCRIPT_DIR} -o daemon.debug -- zappa -p 5678 form.zappa.coffee
--- toy/bob/stop.sh
+++ toy/bob/stop.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 daemon -n sotel-video-portal --stop
