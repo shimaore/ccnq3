c06181bb0fcc9bd7ea138a525e3673e7f6188fb8
Stephane Alnet <stephane@shimaore.net>
2011-02-16 18:35:23 +0100

    fix

--- private/sotelvideo/start.sh
+++ private/sotelvideo/start.sh
@@ -5,4 +5,4 @@ if [ "x${SCRIPT_DIR}" == "x." ]; then
   SCRIPT_DIR=`pwd`
 fi
 
-daemon -n sotel-video-portal -r -D ${SCRIPT_DIR} -o daemon.debug -- form.zappa.coffee
+daemon -n sotel-video-portal -r -D ${SCRIPT_DIR} -o daemon.debug -- ./form.zappa.coffee
