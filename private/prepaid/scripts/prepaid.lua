7fcdd087b7128b48be0e8b74d4a43776fa20013f
Stephane Alnet <stephane@shimaore.net>
2011-03-17 12:51:56 +0100

    finally figured it out, differentiate account vs current state

--- private/prepaid/scripts/prepaid.lua
+++ private/prepaid/scripts/prepaid.lua
@@ -29,6 +29,19 @@ prepaid_uri          = session:getVariable("prepaid_uri")
 
 urlencoded_account   = cgilua.urlcode.escape(prepaid_account)
 
+function get_account()
+  session:execute("curl", prepaid_uri .. "/" .. urlencoded_account )
+  curl_response_code = session:getVariable("curl_response_code")
+  curl_response_data = session:getVariable("curl_response_data")
+
+  freeswitch.consoleLog("DEBUG", "response: "..curl_response_code.." "..curl_response_data)
+
+  if curl_response_code == "200" then
+    return json.decode(curl_response_data)  -- use pcall() if this ends up crashing
+  end
+  return nil
+end
+
 function get_current()
   session:execute("curl", prepaid_uri .. "/_design/prepaid/_view/current?reduce=true&group=true&key=" .. urlencoded_account )
   curl_response_code = session:getVariable("curl_response_code")
@@ -44,6 +57,13 @@ function get_current()
 end
 
 if session:ready() then
+
+  account = get_account()
+  if account == nil then
+    freeswitch.consoleLog("NOTICE", "Account does not exist.\n")
+    session:hangup()
+  end
+
   row = get_current()
   if row == nil or row.value < 2 then
     freeswitch.consoleLog("NOTICE", "No time on account.\n")
@@ -58,6 +78,8 @@ end
 start_time        = os.time()  -- seconds
 recorded_duration = 0          -- seconds
 
+interval_duration = account.interval
+
 function record_interval()
   session:execute("curl", prepaid_uri .. "/" .. urlencoded_account .. " post intervals=1" )
   curl_response_code = session:getVariable("curl_response_code")
@@ -65,7 +87,7 @@ function record_interval()
   freeswitch.consoleLog("DEBUG", "response: "..curl_response_code)
 
   if curl_response_code == "200" then
-    recorded_duration = recorded_duration + row.interval -- seconds
+    recorded_duration = recorded_duration + interval_duration -- seconds
   else
     freeswitch.consoleLog("ERROR", "No access to timer, stopping the call.\n")
     session:hangup()
@@ -82,7 +104,7 @@ while session:ready() do
   actual_duration = os.time()-start_time -- seconds
   -- Offset the next check time to try to keep things in sync, since the
   -- curl code might take a little while to complete.
-  wait_for = row.interval + (recorded_duration - actual_duration) -- seconds
+  wait_for = interval_duration + (recorded_duration - actual_duration) -- seconds
 
   freeswitch.consoleLog("NOTICE", string.format("Waiting for %d seconds\n",wait_for))
   sleep(wait_for*1000-10)
