2112579a9bf7a0ed2bd80d326cdfcda8fadb4755
Stephane Alnet <stephane@shimaore.net>
2011-04-20 21:07:03 +0200

    users.js cleanup

--- couchapps/users.js
+++ couchapps/users.js
@@ -7,7 +7,6 @@ Released under the Affero GPL3 license or above
 //   couchapp push users.js http://127.0.0.1:5984/_users
 
 var couchapp = require('couchapp');
-var path     = require('path');
 
 ddoc = {
     _id: '_design/app'
@@ -26,16 +25,15 @@ ddoc.filters.send_confirmation = function(doc,req) {
 
 ddoc.validate_doc_update = function (newDoc, oldDoc, userCtx) {
 
-  if(doc.status !== 'confirmed') {
+  if(oldDoc.status !== 'confirmed') {
     for (var i in doc.roles) {
-      if( doc.roles[i].match('^account:') ) {
+      if( newDoc.roles[i].match('^account:') ) {
         throw({forbidden : "Only registered users might be granted account access."});
       }
     }
   }
-
-
 }
 
-// Attachments are loaded from portal/*
+// Attachments are loaded from users/*
+// var path     = require('path');
 // couchapp.loadAttachments(ddoc, path.join(__dirname, 'users'));
