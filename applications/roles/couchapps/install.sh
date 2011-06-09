a7b2cadb5c01bae74dca27ba0a14c9929afe00e1
Stephane Alnet <stephane@shimaore.net>
2011-06-09 15:53:04 +0200

    new bootstrap code

--- applications/roles/couchapps/install.sh
+++ applications/roles/couchapps/install.sh
@@ -1,12 +1,15 @@
+#!/usr/bin/env bash
 
 echo "Installing rules into _users."
 coffee -c users.coffee
-couchapp push users.js http://admin:`cat ~/admin.txt`@127.0.0.1:5984/_users
+couchapp push users.js "${CDB_URI}/_users"
+rm users.coffee
 
 echo "Creating CouchDB databases."
-curl -X PUT http://admin:`cat ~/admin.txt`@127.0.0.1:5984/databases
+curl -X PUT "${CDB_URI}/databases"
 
 echo "Installing rules into databases."
 coffee -c databases.js
-couchapp push databases.js http://admin:`cat ~/admin.txt`@127.0.0.1:5984/databases
+couchapp push databases.js "${CDB_URI}/databases"
+rm databases.js
 
