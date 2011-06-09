3d38b8110018f08181a0dfa7a3061a3702e03351
Stephane Alnet <stephane@shimaore.net>
2011-06-09 22:28:57 +0200

    cleaned up "install.sh" for couchapps

--- applications/host/couchapps/install.sh
+++ applications/host/couchapps/install.sh
@@ -1,23 +1,6 @@
+#!/usr/bin/env bash
 
 echo "Installing rules into host."
 coffee -c host.coffee
-couchapp push host.js http://admin:`cat ~/admin.txt`@127.0.0.1:5984/provisioning
-
-curl -u admin:`cat ~/admin.txt` --basic \
-    --request PUT \
-    --header 'Content-Type: application/json' \
-    --data @- \
-    'http://127.0.0.1:5984/log/_security' <<'JSON'
-{
-  "admins": {
-    "names":[],
-    "roles":["log_admin"]
-  },
-  "readers": {
-    "names":[],
-    "roles":["log_reader"]
-  }
-}
-JSON
-
-
+couchapp push host.js "${CDB_URI}/provisioning"
+rm -f host.js
