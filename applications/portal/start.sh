bb1d73ac84f00714c441293fc514f6bd84c7aa5e
Stephane Alnet <stephane@shimaore.net>
2011-06-14 15:20:49 +0200

    need to keep pwd (daemon resets to / by default)

--- applications/portal/start.sh
+++ applications/portal/start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-daemon -n "ccnq3_mail_confirmation" -r -o daemon.debug -- coffee agents/mail_confirmation.coffee
-daemon -n "ccnq3_mail_password"     -r -o daemon.debug -- coffee agents/mail_password.coffee
-daemon -n "ccnq3_portal"            -r -o daemon.debug -- coffee start_portal.coffee
+CHDIR=`pwd`
+daemon -n "ccnq3_mail_confirmation" -D "${CHDIR}" -r -o daemon.debug -- coffee agents/mail_confirmation.coffee
+daemon -n "ccnq3_mail_password"     -D "${CHDIR}" -r -o daemon.debug -- coffee agents/mail_password.coffee
+daemon -n "ccnq3_portal"            -D "${CHDIR}" -r -o daemon.debug -- coffee start_portal.coffee
 
