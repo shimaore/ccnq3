5f4b3ffae2845257ca9fb37ba48df5dc5b9846f7
Stephane Alnet <stephane@shimaore.net>
2011-06-16 02:19:35 +0200

    updated installation doc; updated bootstrap scripts

--- applications/prepaid/couchapps/install.sh
+++ applications/prepaid/couchapps/install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Creating CouchDB prepaid."
+curl -X PUT "${CDB_URI}/prepaid"
+
 echo "Installing prepaid couchapp."
 coffee -c prepaid.coffee
 couchapp push prepaid.js "${CDB_URI}/prepaid"
