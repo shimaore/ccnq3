6a5dd17ac49033807ff8030d5090a879d942430c
Stephane Alnet <stephane@shimaore.net>
2011-09-07 00:40:11 +0200

    Differentiate start operations so that the minimalist configuration done by bootstrap-system works. In this case, start_admin would not start without a port number. But actually we don't need to start it so early.

--- applications/roles/start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-CHDIR=`pwd`
-daemon -n "ccnq3_track_users"     -D "${CHDIR}/agents" -r -o daemon.debug -- coffee track_users.coffee
-daemon -n "ccnq3_admin"           -D "${CHDIR}/node"   -r -o daemon.debug -- coffee start_admin.coffee
--- applications/roles/stop.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-daemon -n "ccnq3_admin"             -o daemon.debug --stop
-daemon -n "ccnq3_track_users"       -o daemon.debug --stop
