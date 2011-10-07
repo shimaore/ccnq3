389999ba44f4955573aeaf0e53f6495294032133
Stephane Alnet <stephane@shimaore.net>
2011-10-07 15:10:24 +0200

    opensips install

--- common/opensips/install.sh
+++ common/opensips/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo aptitude install -y opensips opensips-dbhttp-module opensips-json-module
+sudo adduser `whoami` opensips # allows access to fifo
+
 sudo sed -i -e 's/RUN_OPENSIPS=no/RUN_OPENSIPS=yes/' /etc/default/opensips
 echo 'TZ=UTC' | sudo tee -a /etc/default/opensips
 
