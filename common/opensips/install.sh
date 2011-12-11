836ffc0e153b0acfafe90bb0b311b1754364ecb4
Stephane Alnet <stephane@shimaore.net>
2011-12-11 23:58:22 +0100

    common/opensips: moved out Perl code; split most of the compiler into a separate module

--- common/opensips/install.sh
+++ common/opensips/install.sh
@@ -6,6 +6,6 @@ sudo adduser `whoami` opensips # allows access to fifo
 sudo sed -i -e 's/RUN_OPENSIPS=no/RUN_OPENSIPS=yes/' /etc/default/opensips
 echo 'TZ=UTC' | sudo tee -a /etc/default/opensips
 
-sudo ./make.pl default.json conference.json local-vars.json
+sudo ./make.coffee default.json conference.json local-vars.json
 
 sudo /etc/init.d/opensips restart
