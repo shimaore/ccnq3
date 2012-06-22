635561d80ebe3e2668ece866c3b1bf1ae47fbf99
Stephane Alnet <stephane@shimaore.net>
2012-06-22 11:19:16 +0200

    Stashed away unsupported code.

--- applications/prepaid/start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-CHDIR=`pwd`
-daemon -n "ccnq3_prepaid"     -D "${CHDIR}/node" -r -o daemon.debug -- coffee start_prepaid.coffee
-daemon -n "ccnq3_esl_prepaid" -D "${CHDIR}/node" -r -o daemon.debug -- coffee esl_server.coffee
--- applications/prepaid/stop.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-daemon -n "ccnq3_esl_prepaid" -o daemon.debug --stop
-daemon -n "ccnq3_prepaid"     -o daemon.debug --stop
