fb5989fcc457d2e0fa4c61bb2534d2ff1ff3b25f
Stephane Alnet <stephane@shimaore.net>
2012-10-31 22:17:37 +0100

    Corrected path in couchdb config. CouchDB rewrites the path by removing the matching prefix, so we need to add it back.

--- bin/bootstrap-manager.sh
+++ bin/bootstrap-manager.sh
@@ -59,7 +59,7 @@ require_valid_user = true
 level = error
 
 [httpd_global_handlers]
-_ccnq3 = {couch_httpd_proxy, handle_proxy_req, <<"http://127.0.0.1:35984">>}
+_ccnq3 = {couch_httpd_proxy, handle_proxy_req, <<"http://127.0.0.1:35984/_ccnq3">>}
 
 [admins]
 admin = ${ADMIN_PASSWORD}
