782d18d56a212e36b626cbb56c62f538490a3d6a
Stephane Alnet <stephane@shimaore.net>
2013-10-15 17:10:39 +0200

    updated for recent en-us-callie sound package layout (different rates are built on the target)

--- test/freeswitch/start.sh
+++ test/freeswitch/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="`pwd`"
 OK=true
-for PACKAGE in freeswitch freeswitch-mod-event-socket freeswitch-mod-hash freeswitch-mod-commands freeswitch-mod-dptools freeswitch-mod-loopback freeswitch-mod-console freeswitch-mod-logfile freeswitch-mod-dialplan-xml freeswitch-mod-sofia freeswitch-mod-sndfile freeswitch-sounds-en-us-callie-8000 freeswitch-sounds-fr-fr-sibylle-8000; do
+for PACKAGE in freeswitch freeswitch-mod-event-socket freeswitch-mod-hash freeswitch-mod-commands freeswitch-mod-dptools freeswitch-mod-loopback freeswitch-mod-console freeswitch-mod-logfile freeswitch-mod-dialplan-xml freeswitch-mod-sofia freeswitch-mod-sndfile freeswitch-sounds-en-us-callie freeswitch-sounds-fr-fr-sibylle-8000; do
   dpkg -l "${PACKAGE}" > /dev/null 2>&1 || { echo "Missing $PACKAGE"; OK=false; }
 done
 if [ "$OK" == "false" ]; then exit 1; fi
