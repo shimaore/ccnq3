68a919984843cb9a31743a4955bcf91b7bedae72
Stephane Alnet <stephane@shimaore.net>
2013-12-09 12:57:31 +0100

    test: no stats, no media

--- test/freeswitch-release/start.sh
+++ test/freeswitch-release/start.sh
@@ -16,22 +16,22 @@ killall sipp
 sipp -bg \
   -d 1500 -nostdin -timeout 40 \
   -l 1 -m 1 -r 1 \
-  -log_file uac.log -error_file uac-error.log -message_file uac-message.log -stf uac-stats.log -trace_msg uac-trace.log \
-  -trace_msg -trace_screen -trace_err -trace_stat -trace_logs \
-  -i 127.0.0.1 -mi 127.0.0.1 -rtp_echo \
+  -log_file uac.log -error_file uac-error.log -message_file uac-message.log -trace_msg uac-trace.log \
+  -trace_msg -trace_screen -trace_err -trace_logs \
+  -i 127.0.0.1 -mi 127.0.0.1 \
   -bind_local -default_behaviors none -nd -fd 1 \
-  -sf uac-with-reinvite.xml -p 15060 -mp 25060 \
+  -sf uac-with-reinvite.xml -p 15060 \
   -s 163578273827 127.0.0.1:5060
 
 # UAS
 sipp -bg \
   -d 1500 -nostdin -timeout 40 \
   -l 1 -m 1 -r 1 \
-  -log_file uas.log -error_file uas-error.log -message_file uas-message.log -stf uas-stats.log -trace_msg uas-trace.log \
-  -trace_msg -trace_screen -trace_err -trace_stat -trace_logs \
-  -i 127.0.0.1 -mi 127.0.0.1 -rtp_echo \
+  -log_file uas.log -error_file uas-error.log -message_file uas-message.log -trace_msg uas-trace.log \
+  -trace_msg -trace_screen -trace_err -trace_logs \
+  -i 127.0.0.1 -mi 127.0.0.1 \
   -bind_local -default_behaviors none -nd -fd 1 \
-  -sf uas-with-reinvite.xml -p 15062 -mp 25062 \
+  -sf uas-with-reinvite.xml -p 15062 \
   127.0.0.1:5060
 
 freeswitch -c -nonat -nonatmap -nort \
